133c0de9457e53878aa61fc21aed4c93735a50a9
Admin <156>
2023-03-08 20:32:10 +0200

    third try to commit

--- app/src/main/java/com/example/udemygitstart/MainActivity.kt
+++ app/src/main/java/com/example/udemygitstart/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.udemygitstart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var stringHelloText = "hello text"
+
+    }
+    fun createNewFeatureOfBasic(){
+        var texViewNew = findViewById<TextView>(R.id.textVv)
     }
 }
